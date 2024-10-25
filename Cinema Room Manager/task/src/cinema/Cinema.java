3e9e542291b1855cb21b54c6609a82b724358732
Nick Bauters (7dbb) <nick.bauters@colruytgroup.com>
2024-10-25 15:13:37 +0200

    feat: Calculate flat fee of seats without special cases or large screening rooms

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,9 +1,19 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        ScreenRoom room = new ScreenRoom();
-        System.out.println(room);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the number of seats in each row:");
+        int seatsPerRow = Integer.parseInt(scanner.nextLine());
+
+        int income = rows * seatsPerRow * 10;
+
+        System.out.println("Total income:");
+        System.out.println("$" + income);
     }
 }
